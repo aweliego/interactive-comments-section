3f468d74c75f13d090efee70c8d8d1fd2e5168e3
“Amélie <amelie.gorry.am@gmail.com>
2023-12-10 10:07:41 +0100

    Customize tailwind.config with design styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          blue: {
+            light: 'hsl(239, 57%, 85%)',
+            moderate: 'hsl(238, 40%, 52%)'
+          },
+          red: {
+            soft: 'hsl(358, 79%, 66%)',
+            pale: 'hsl(357, 100%, 86%)'
+          }
+        },
+        neutral: {
+          blue: {
+            dark: 'hsl(212, 24%, 26%)',
+            grayish: 'hsl(211, 10%, 45%)'
+          },
+          gray: {
+            light: 'hsl(223, 19%, 93%)',
+            "extra-light": 'hsl(228, 33%, 97%)'
+          },
+          white: 'hsl(0, 0%, 100%)'
+        }
+      },
+      fontFamily: {
+        body: ['Rubik'],
+      },
+      screens: {
+        sm: '376px',
+        lg: '1440px'
+      },
+    },
   },
   plugins: [],
 }
