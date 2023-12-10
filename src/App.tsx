ddcbcdae8604df063fc8b391b18f2e4ba551140f
“Amélie <amelie.gorry.am@gmail.com>
2023-12-10 12:10:37 +0100

    Style App and Comment for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   const { comments } = data
 
   return (
-    <section>
+    <section className='flex flex-col items-center justify-center w-5/12 mx-auto p-10'>
       {comments.map((comment) => <Comment key={comment.id} {...comment} />)}
     </section>
   )
--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,3 +1,7 @@
+import IconPlus from '../../../public/images/icon-plus.svg'
+import IconMinus from '../../../public/images/icon-minus.svg'
+import IconReply from '../../../public/images/icon-reply.svg'
+
 interface MessageMeta {
     id: number
     content: string
@@ -21,21 +25,39 @@ const Comment: React.FC<CommentInterface> = ({ id, content, createdAt, score, us
 
     return (
         <>
-            <article>
-                <div>
-                    <p>{score}</p>
+            <article className="bg-neutral-white flex flex-col p-8 m-2 rounded w-full">
+                <div className='flex'>
+                    {/* Score */}
+                    <div className="bg-neutral-gray-extra-light flex flex-col items-center py-4 px-3 mr-6 rounded h-max">
+                        <img src={IconPlus} className='text-neutral-gray-light w-3 hover:cursor-pointer' />
+                        <p className="text-primary-blue-moderate font-medium py-4">{score}</p>
+                        <img src={IconMinus} className='text-neutral-gray-light w-3 hover:cursor-pointer' />
+                    </div>
+                    {/* Meta and CTA */}
+                    <div className='flex flex-col'>
+                        <div className='flex mb-4'>
+                            {/* Meta */}
+                            <div className='flex flex-grow-1 w-full gap-4 items-center'>
+                                <img src={user.image.png} alt="user-icon" className='w-8' />
+                                <p className='text-neutral-blue-dark font-semibold'>{user.username}</p>
+                                <p className='text-neutral-blue-grayish'>{createdAt}</p>
+                            </div>
+                            {/* CTA */}
+                            <div className='text-primary-blue-moderate  flex flex-grow-0 items-center gap-2 mr-4 hover:cursor-pointer'>
+                                <img src={IconReply} alt="" className='w-4' />
+                                <p className="hover:text-primary-blue-light font-medium">  Reply</p>
+                            </div>
+                        </div>
+                        {/* Content */}
+                        <div className='text-neutral-blue-grayish'>
+                            <p>{content}</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <p>{user.username}</p>
-                    <p>{createdAt}</p>
-                    <p>Reply</p>
-                </div>
-                <div>
-                    <p>{content}</p>
-                </div>
-                <div>
+                {/* Replies */}
+                {/* <div>
                     {replies.length > 0 && replies.map(reply => (<div>{reply.content}</div>))}
-                </div>
+                </div> */}
             </article>
         </>
     )
