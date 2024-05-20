677dc7348dfa9909585fa0f8aac244beed320aae
“Amélie <amelie.gorry.am@gmail.com>
2024-05-20 14:23:01 +0200

    Fix tests

--- src/components/Comment/__test__/Comment.test.tsx
+++ src/components/Comment/__test__/Comment.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { render, screen, waitFor, within } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../../../App'
 
@@ -31,60 +31,51 @@ describe('Comment component', () => {
     })
   })
 
-  describe('when a user posts a new comment', () => {
+  describe('when a user posts a new comment', async () => {
     it('should render a new comment after clicking the Send button of the bottom comment form', async () => {
       const user = userEvent.setup()
       render(<MockApp />)
 
-      const textbox = screen.getAllByPlaceholderText(/add a comment/i)
-      const sendBtn = screen.getAllByRole('button', {
-        name: /send/i
-      })
-      user.type(textbox[0], 'Posting a new comment!')
-      user.click(sendBtn[0])
+      // Targeting last textarea and send button (NewCommentForm)
+      const textboxes = screen.getAllByPlaceholderText(/add a comment/i)
+      const textbox = textboxes[textboxes.length - 1]
+      const sendBtns = screen.getAllByRole('button', { name: /send/i })
+      const sendBtn = sendBtns[sendBtns.length - 1]
 
-      await waitFor(() => {
-        const newCommentContent = screen.queryByRole('article', { name: /posting/i })
-        if (newCommentContent) {
-          expect(newCommentContent).toBeInTheDocument()
-        }
-      })
+      await user.type(textbox, 'Posting a new comment!')
+      await user.click(sendBtn)
+
+      const newCommentContent = await screen.findByText('Posting a new comment!')
+      expect(newCommentContent).toBeInTheDocument()
     })
 
     it('should disable the Send button if the comment content is blank', async () => {
       const user = userEvent.setup()
       render(<MockApp />)
 
-      const textbox = screen.getAllByPlaceholderText(/add a comment/i)
+      const textbox = screen.getAllByPlaceholderText(/add a comment/i)[0]
       const sendBtn = screen.getAllByRole('button', {
         name: /send/i
-      })
-      user.type(textbox[0], '  ')
-      expect(sendBtn[0]).toBeDisabled()
+      })[0]
+      user.type(textbox, '  ')
+      expect(sendBtn).toBeDisabled()
     })
   })
 
-  describe('when a user replies to a comment', () => {
+  describe('when a user replies to a comment', async () => {
     it('should render a new reply after clicking the Reply button', async () => {
       const user = userEvent.setup()
       render(<MockApp />)
-      const replyActionBtn = screen.getAllByText(/reply/i)
-      const textbox = screen.getAllByPlaceholderText(/add a comment/i)
-      user.click(replyActionBtn[0])
-      user.type(textbox[0], 'Replying to a comment!')
-
-      await waitFor(() => {
-        const replySubmitBtn = screen.queryByRole('button', {
-          name: /reply/i
-        })
-        if (replySubmitBtn) {
-          user.click(replySubmitBtn)
-        }
-        const newReplyContent = screen.queryByRole('article', { name: /replying/i })
-        if (newReplyContent) {
-          expect(newReplyContent).toBeInTheDocument()
-        }
+      const replyActionBtn = screen.getAllByText(/reply/i)[1]
+      const textbox = screen.getAllByPlaceholderText(/add a comment/i)[1]
+      await user.click(replyActionBtn)
+      await user.type(textbox, 'Replying to a comment!')
+      const replySubmitBtn = await screen.findByRole('button', {
+        name: /reply/i
       })
+      await user.click(replySubmitBtn)
+      const newReplyContent = await screen.findByText('Replying to a comment!')
+      expect(newReplyContent).toBeInTheDocument()
     })
   })
 
@@ -92,16 +83,18 @@ describe('Comment component', () => {
 
     it('should not allow user to edit comments from other users', () => {
       render(<MockApp />)
-      const commentOtherUser = screen.getByText(/amyrobson/i)
-      const editBtn = within(commentOtherUser).queryByText(/edit/i)
-      expect(editBtn).toBeNull()
+      const commentOtherUser = screen.getByText(/amyrobson/i).closest('article')
+      if (commentOtherUser) {
+        const editBtn = within(commentOtherUser).queryByText(/edit/i)
+        expect(editBtn).toBeNull()
+      }
     })
   })
 
   it('should turn the comment into a text area with an update button', async () => {
     const user = userEvent.setup()
     render(<MockApp />)
-    const commentCurrentUser = screen.getByText(/juliusomo/i).closest('article')
+    const commentCurrentUser = screen.getAllByText(/juliusomo/i)[0].closest('article')
     expect(commentCurrentUser).toBeInTheDocument()
 
     if (commentCurrentUser) {
@@ -118,29 +111,27 @@ describe('Comment component', () => {
     }
   })
 
-
   it('should render the edited comment with the new edited content', async () => {
     const user = userEvent.setup()
     render(<MockApp />)
 
-    const commentCurrentUser = screen.getByText(/juliusomo/i).closest('article')
+    // Get a comment from the current user 'juliusomo'
+    const commentCurrentUser = screen.getAllByText(/juliusomo/i)[0].closest('article')
     expect(commentCurrentUser).toBeInTheDocument()
 
     if (commentCurrentUser) {
       const editActionBtn = within(commentCurrentUser).getByText(/edit/i)
-      user.click(editActionBtn)
+      await user.click(editActionBtn)
 
       const textbox = await within(commentCurrentUser).findByRole('textbox')
-      const submitBtn = await within(commentCurrentUser).findByRole('button', {
-        name: /update/i
-      })
+      const submitBtn = await within(commentCurrentUser).findByRole('button', { name: /update/i })
+
+      await user.clear(textbox)
       await user.type(textbox, 'Editing my comment!')
       await user.click(submitBtn)
 
-      await waitFor(() => {
-        const editedContent = screen.queryByRole('article', { name: /editing/i })
-        editedContent && expect(editedContent).toBeInTheDocument()
-      })
+      const editedContent = await within(commentCurrentUser).findByText('Editing my comment!')
+      expect(editedContent).toBeInTheDocument()
     }
   })
 
