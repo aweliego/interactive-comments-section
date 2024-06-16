e31d38855aff26ca407e432ca63429ca1616758e
“Amélie <amelie.gorry.am@gmail.com>
2024-06-16 13:01:25 +0200

    Add tests for Delete functionality + refactor to more DRY code

--- src/components/Comment/__test__/Comment.test.tsx
+++ src/components/Comment/__test__/Comment.test.tsx
@@ -1,7 +1,13 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll, beforeEach, vi } from 'vitest'
 import { render, screen, within } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import userEvent, { UserEvent } from '@testing-library/user-event'
 import App from '../../../App'
+
+// Mock the HTMLDialogElement methods
+beforeAll(() => {
+  HTMLDialogElement.prototype.showModal = vi.fn()
+  HTMLDialogElement.prototype.close = vi.fn()
+})
 
 const MockApp = () => {
   return (
@@ -10,10 +16,21 @@ const MockApp = () => {
 }
 
 describe('Comment component', () => {
+
+  let user: UserEvent, commentCurrentUser: HTMLElement | null, commentOtherUser: HTMLElement | null
+
+  beforeEach(() => {
+    user = userEvent.setup()
+    render(<MockApp />)
+    commentCurrentUser = screen.getAllByText(/juliusomo/i)[0].closest('article')
+    expect(commentCurrentUser).toBeInTheDocument()
+    commentOtherUser = screen.getByText(/amyrobson/i).closest('article')
+    expect(commentOtherUser).toBeInTheDocument()
+  })
+
+
   describe('when component initially renders', () => {
     it('should show the initial comments and replies correctly', () => {
-      render(<MockApp />)
-
       const commentContent = screen.getByText(/impressive/i)
       const commentUser = screen.getByText(/amyrobson/i)
       const commentReplies = screen.getAllByText(/recommend/i)
@@ -25,17 +42,13 @@ describe('Comment component', () => {
     })
 
     it('should not show the reply section when there are no replies', () => {
-      render(<MockApp />)
       const commentReplies = screen.queryByText('This is a reply')
       expect(commentReplies).toBeNull()
     })
   })
 
-  describe('when a user posts a new comment', async () => {
+  describe('when a user posts a new comment', () => {
     it('should render a new comment after clicking the Send button of the bottom comment form', async () => {
-      const user = userEvent.setup()
-      render(<MockApp />)
-
       // Targeting last textarea and send button (NewCommentForm)
       const textboxes = screen.getAllByPlaceholderText(/add a comment/i)
       const textbox = textboxes[textboxes.length - 1]
@@ -50,9 +63,6 @@ describe('Comment component', () => {
     })
 
     it('should disable the Send button if the comment content is blank', async () => {
-      const user = userEvent.setup()
-      render(<MockApp />)
-
       const textbox = screen.getAllByPlaceholderText(/add a comment/i)[0]
       const sendBtn = screen.getAllByRole('button', {
         name: /send/i
@@ -62,10 +72,8 @@ describe('Comment component', () => {
     })
   })
 
-  describe('when a user replies to a comment', async () => {
+  describe('when a user replies to a comment', () => {
     it('should render a new reply after clicking the Reply button', async () => {
-      const user = userEvent.setup()
-      render(<MockApp />)
       const replyActionBtn = screen.getAllByText(/reply/i)[1]
       const textbox = screen.getAllByPlaceholderText(/add a comment/i)[1]
       await user.click(replyActionBtn)
@@ -80,60 +88,96 @@ describe('Comment component', () => {
   })
 
   describe('when a user edits their comment', () => {
+    let editActionBtn: HTMLElement | null
+    beforeEach(() => {
+      if (commentCurrentUser) {
+        editActionBtn = within(commentCurrentUser).getByText(/edit/i)
+        expect(editActionBtn).toBeInTheDocument()
+      }
+    })
+
+    const setupEdit = async () => {
+      if (commentCurrentUser && editActionBtn) {
+        await user.click(editActionBtn)
+        const textbox = await within(commentCurrentUser).findByRole('textbox')
+        const submitBtn = await within(commentCurrentUser).findByRole('button', { name: /update/i })
+        return { textbox, submitBtn }
+      }
+      return {}
+    }
 
     it('should not allow user to edit comments from other users', () => {
-      render(<MockApp />)
-      const commentOtherUser = screen.getByText(/amyrobson/i).closest('article')
       if (commentOtherUser) {
-        const editBtn = within(commentOtherUser).queryByText(/edit/i)
-        expect(editBtn).toBeNull()
+        editActionBtn = within(commentOtherUser).queryByText(/edit/i)
+        expect(editActionBtn).toBeNull()
       }
+    })
+
+    describe('Edit comment functionality', () => {
+      it('should turn the comment into a text area with an update button', async () => {
+        const { textbox, submitBtn } = await setupEdit()
+        expect(textbox).toBeInTheDocument()
+        expect(submitBtn).toBeInTheDocument()
+      })
+
+      it('should render the edited comment with the new edited content', async () => {
+        const { textbox, submitBtn } = await setupEdit()
+        if (textbox && submitBtn) {
+          await user.clear(textbox)
+          await user.type(textbox, 'Editing my comment!')
+          await user.click(submitBtn)
+        }
+
+        if (commentCurrentUser) {
+          const editedContent = await within(commentCurrentUser).findByText('Editing my comment!')
+          expect(editedContent).toBeInTheDocument()
+        }
+      })
     })
   })
 
-  it('should turn the comment into a text area with an update button', async () => {
-    const user = userEvent.setup()
-    render(<MockApp />)
-    const commentCurrentUser = screen.getAllByText(/juliusomo/i)[0].closest('article')
-    expect(commentCurrentUser).toBeInTheDocument()
 
-    if (commentCurrentUser) {
-      const editActionBtn = within(commentCurrentUser).getByText(/edit/i)
-      user.click(editActionBtn)
+  describe('when a user deletes a comment', () => {
+    let deleteActionBtn: HTMLElement | null
+    it('should not allow user to delete comments from other users', () => {
+      if (commentOtherUser) deleteActionBtn = within(commentOtherUser).queryByText(/delete/i)
+      expect(deleteActionBtn).toBeNull()
+    })
 
-      const textbox = await within(commentCurrentUser).findByRole('textbox')
-      const submitBtn = await within(commentCurrentUser).findByRole('button', {
-        name: /update/i
+    describe('Confirmation modal', () => {
+      let modal: HTMLElement | null
+      beforeEach(() => {
+        modal = screen.queryAllByRole('dialog', { hidden: true })[0]
+        expect(modal).toBeInTheDocument()
       })
 
-      expect(textbox).toBeInTheDocument()
-      expect(submitBtn).toBeInTheDocument()
-    }
+      it('should open a confirmation modal when the Delete button is clicked', async () => {
+        if (commentCurrentUser) deleteActionBtn = within(commentCurrentUser).getByText(/^\bdelete\b$/i)
+        if (deleteActionBtn) {
+          await user.click(deleteActionBtn)
+          expect(HTMLDialogElement.prototype.showModal).toHaveBeenCalled()
+        }
+      })
+
+      it('should delete the comment when the "Yes, delete" button is clicked', async () => {
+        if (modal) {
+          const confirmDeleteBtn = within(modal).getByText(/^Yes, delete$/i)
+          expect(confirmDeleteBtn).toBeInTheDocument()
+          await user.click(confirmDeleteBtn)
+          expect(commentCurrentUser).not.toBeInTheDocument()
+        }
+      })
+
+      it('should close the modal without deleting when the "No, cancel" button is clicked', async () => {
+        if (modal) {
+          const cancelBtn = within(modal).getByText(/^No, cancel$/i)
+          expect(cancelBtn).toBeInTheDocument()
+          await user.click(cancelBtn)
+          expect(HTMLDialogElement.prototype.close).toHaveBeenCalled()
+          expect(commentCurrentUser).toBeInTheDocument()
+        }
+      })
+    })
   })
-
-  it('should render the edited comment with the new edited content', async () => {
-    const user = userEvent.setup()
-    render(<MockApp />)
-
-    // Get a comment from the current user 'juliusomo'
-    const commentCurrentUser = screen.getAllByText(/juliusomo/i)[0].closest('article')
-    expect(commentCurrentUser).toBeInTheDocument()
-
-    if (commentCurrentUser) {
-      const editActionBtn = within(commentCurrentUser).getByText(/edit/i)
-      await user.click(editActionBtn)
-
-      const textbox = await within(commentCurrentUser).findByRole('textbox')
-      const submitBtn = await within(commentCurrentUser).findByRole('button', { name: /update/i })
-
-      await user.clear(textbox)
-      await user.type(textbox, 'Editing my comment!')
-      await user.click(submitBtn)
-
-      const editedContent = await within(commentCurrentUser).findByText('Editing my comment!')
-      expect(editedContent).toBeInTheDocument()
-    }
-  })
-
 })
 
